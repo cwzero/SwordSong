4309154844dd6367b00e1b18d6d73efb12c5e156
Eric Gjerde <gjerdeeric@gmail.com>
2022-05-05 20:44:59 -0500

    Add Grid2D

--- Include/Phantasia/Math/TwoD/Grid2D.hpp
+++ Include/Phantasia/Math/TwoD/Grid2D.hpp
@@ -5,11 +5,12 @@
 #include "Phantasia/Math/TwoD/Rect2D.hpp"
 
 namespace Phantasia::Math::TwoD {
+    template <typename T>
     struct Grid2D : public Rect2D {
         Grid2D(int x, int y, int width, int height);
         Grid2D(Point2D loc, Size2D size);
 
-        
+        T& get(Point2D loc);
     private:
         
     };
